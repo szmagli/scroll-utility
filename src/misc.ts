f549f8ddda6024ba59b23ff59323ff52915dc149
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-10-02 20:07:43 +0000

    refactoring

--- src/misc.ts
+++ src/misc.ts
@@ -20,74 +20,65 @@ interface SElement {
 }
 
 function getElementWrapper(el: HTMLElement | Window, horizontal: boolean): SElement {
-	function wWindow<T>(fw: (element: Window) => T, fe: (element: HTMLElement) => T) {
-		return isWindow(el) ? fw(el) : fe(el)
-	}
-	const result = {
+	return {
 		element: el,
 		horizontal,
-		size: wWindow(
-			() =>
-				horizontal
-					? () => documentElement.clientWidth || document.body.clientWidth || window.innerWidth
-					: () => documentElement.clientHeight || document.body.clientHeight || window.innerHeight,
-			el => (horizontal ? () => el.clientWidth : () => el.clientHeight),
-		),
-		scrollSize() {
-			return (
-				wWindow(
-					() =>
-						horizontal
-							? () =>
-									Math.max(
-										document.body.scrollWidth,
-										document.body.offsetWidth,
-										documentElement.clientWidth,
-										documentElement.scrollWidth,
-										documentElement.offsetWidth,
-									)
-							: () =>
-									Math.max(
-										document.body.scrollHeight,
-										document.body.offsetHeight,
-										documentElement.clientHeight,
-										documentElement.scrollHeight,
-										documentElement.offsetHeight,
-									),
-					el => (horizontal ? () => el.scrollWidth : () => el.scrollHeight),
-				)() - result.size()
-			)
-		},
-		scrollPosition: wWindow(
-			() => (horizontal ? () => window.pageXOffset : () => window.pageYOffset),
-			el => (horizontal ? () => el.scrollLeft : () => el.scrollTop),
-		),
-		offset: wWindow(
-			() =>
-				horizontal
-					? () => documentElement.clientWidth || document.body.clientWidth || window.innerWidth
-					: () => documentElement.clientHeight || document.body.clientHeight || window.innerHeight,
-			el => (horizontal ? () => el.offsetWidth : () => el.offsetHeight),
-		),
-		border: wWindow(
-			() => () => 0,
-			el =>
-				horizontal
-					? () =>
-							parseInt(getComputedStyle(el, null).getPropertyValue("border-left-width"), 10) || 0
-					: () =>
-							parseInt(getComputedStyle(el, null).getPropertyValue("border-top-width"), 10) || 0,
-		),
-		relativePosition: wWindow(
-			() => () => 0,
-			el =>
-				horizontal ? () => el.getBoundingClientRect().left : () => el.getBoundingClientRect().top,
-		),
+		size: isWindow(el)
+			? horizontal
+				? () => documentElement.clientWidth || document.body.clientWidth || window.innerWidth
+				: () => documentElement.clientHeight || document.body.clientHeight || window.innerHeight
+			: horizontal
+			? () => el.clientWidth
+			: () => el.clientHeight,
+		scrollSize: isWindow(el)
+			? horizontal
+				? () =>
+						Math.max(
+							document.body.scrollWidth,
+							document.body.offsetWidth,
+							documentElement.clientWidth,
+							documentElement.scrollWidth,
+							documentElement.offsetWidth,
+						)
+				: () =>
+						Math.max(
+							document.body.scrollHeight,
+							document.body.offsetHeight,
+							documentElement.clientHeight,
+							documentElement.scrollHeight,
+							documentElement.offsetHeight,
+						)
+			: horizontal
+			? () => el.scrollWidth
+			: () => el.scrollHeight,
+		scrollPosition: isWindow(el)
+			? horizontal
+				? () => window.pageXOffset
+				: () => window.pageYOffset
+			: horizontal
+			? () => el.scrollLeft
+			: () => el.scrollTop,
+		offset: isWindow(el)
+			? horizontal
+				? () => documentElement.clientWidth || document.body.clientWidth || window.innerWidth
+				: () => documentElement.clientHeight || document.body.clientHeight || window.innerHeight
+			: horizontal
+			? () => el.offsetWidth
+			: () => el.offsetHeight,
+		border: isWindow(el)
+			? () => 0
+			: horizontal
+			? () => parseInt(getComputedStyle(el, null).getPropertyValue("border-left-width"), 10) || 0
+			: () => parseInt(getComputedStyle(el, null).getPropertyValue("border-top-width"), 10) || 0,
+		relativePosition: isWindow(el)
+			? () => 0
+			: horizontal
+			? () => el.getBoundingClientRect().left
+			: () => el.getBoundingClientRect().top,
 		scrollBy: horizontal
 			? (value: number) => el.scrollBy(value, 0)
 			: (value: number) => el.scrollBy(0, value),
 	}
-	return result
 }
 
 function isWindow(element: Element | Window): element is Window {
