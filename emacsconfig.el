82206b3d2a711230df683a6caf2ad07d56efb1d3
David Perez Alvarez <david@leddgroup.com>
2019-10-04 23:39:26 -0400

    Updates

--- emacsconfig.el
+++ emacsconfig.el
@@ -2,6 +2,11 @@
 (defun org-babel-execute:pug (body params)
 	(shell-command-to-string (concat "echo $'"  body "' | pug")))
 
+;; (defun after-org  (progn (org-html-export-to-html) (shell-command "mv README.html index.html")))
+;; (add-hook 'org-mode-hook
+;;   (lambda ()
+;;     (add-hook 'after-save-hook  'after-org nil 'make-it-local)))
+
 ;; (defun org-babel-execute:tsx (body params)
 ;; 	(shell-command-to-string (concat "cat > ")))
 
