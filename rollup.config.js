9492c65b52f89b49e079ef32f11794fdbef07065
Daniel Perez Alvarez <danielpza@protonmail.com>
2020-01-18 01:05:27 -0500

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,38 @@
 import typescript from "@rollup/plugin-typescript";
 
+function build({ input, name, file }) {
+	return {
+		input,
+		output: [
+			{
+				name,
+				file: `dist/${file}.iife.js`,
+				format: "iife"
+			},
+			{
+				file: `dist/${file}.cjs.js`,
+				format: "cjs"
+			},
+			{
+				file: `dist/${file}.esm.js`,
+				format: "esm"
+			}
+		],
+		plugins: [typescript()]
+	};
+}
+
 export default () => {
 	return [
-		{
+		build({
 			input: "src/index.ts",
-			output: [
-				{
-					name: "ScrollUtility",
-					file: "dist/scroll-utility.iife.js",
-					format: "iife"
-				},
-				{
-					file: "dist/scroll-utility.cjs.js",
-					format: "cjs"
-				},
-				{
-					file: "dist/scroll-utility.esm.js",
-					format: "esm"
-				}
-			],
-			plugins: [typescript()]
-		}
+			name: "ScrollUtility",
+			file: "scroll-utility"
+		}),
+		build({
+			input: "src/easings.ts",
+			name: "ScrollUtilityEasings",
+			file: "scroll-utility-easings"
+		})
 	];
 };
