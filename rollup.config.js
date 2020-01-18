af18cbf184e7d6707c33c26581ab21aac30de4a0
David Perez Alvarez <david@leddgroup.com>
2020-01-17 22:59:09 -0500

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,15 @@
-import typescript from "rollup-plugin-typescript"
-import html from "rollup-plugin-bundle-html"
-import resolve from "rollup-plugin-node-resolve"
-import commonjs from "rollup-plugin-commonjs"
-import serve from "rollup-plugin-serve"
-import pkg from "./package.json"
+import typescript from "@rollup/plugin-typescript";
 
-export default args => {
+export default () => {
 	return [
-		{
-			input: "doc/index.ts",
-			output: {
-				file: 'doc/index.js',
-				format: "iife",
-			},
-			plugins: [typescript(),],
-		},
 		{
 			input: "src/index.ts",
 			output: {
 				name: "ScrollUtility",
-				file: 'dist/bundle.js',
-				format: "umd",
-			}
+				file: "dist/scroll-utility.js",
+				format: "umd"
+			},
+			plugins: [typescript()]
 		}
 	];
-}
+};
