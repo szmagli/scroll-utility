eed671cc84ed093c965cfbe4a987d977b8362ee5
David Perez Alvarez <david@leddgroup.com>
2020-01-17 22:44:26 -0500

    wip

--- docs/setup.js
+++
@@ -1,8 +0,0 @@
-import * as Prefabs from "../docs/globals";
-import ScrollUtility from "../src";
-
-Object.keys(Prefabs).forEach(key => {
-	global[key] = Prefabs[key];
-});
-
-global.ScrollUtility = ScrollUtility;
